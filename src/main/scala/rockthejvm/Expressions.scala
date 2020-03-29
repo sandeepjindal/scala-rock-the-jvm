95d36023906cfca05306d8ce636c0799f80828fe
sjindal <sandeep.jindal@agoda.com>
2020-03-29 13:32:39 +0700

    Expressions

--- src/main/scala/rockthejvm/Expressions.scala
+++ src/main/scala/rockthejvm/Expressions.scala
@@ -1,13 +1,40 @@
 package rockthejvm
 
-object IIExpressions extends App {
+object Expressions extends App {
 
   // mathematical expression
   val x = 1 + 2
 
-  print(x)
+  println(x)
 
   //relational expression
-  print(1 == x)
+  println(1 == x)
+
+  // Instructions(DO) vs Expressions(have some value)
+
+  val aCondition = true
+
+  val aConditionValue = if(aCondition) 5 else 3 // IF expression
+
+  println(aConditionValue)
+
+  println(if(aCondition) 5 else 3)
+
+
+  // Golden Rule everything in scala is expression.
+  var aVariable =1
+
+  val aWierdValue = (aVariable = 3)
+  println(aWierdValue) // return units
+
+  // Anything expression that returns UNIT is a side effect
+
+
+  //codeBlock Expressions
+  val codeBlock = {
+    val y =1
+    val z = y+1
+    if(z>1) "frost" else "sandeep"
+  }
 
 }
